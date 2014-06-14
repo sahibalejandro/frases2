582497e1ea849dd2a3a87b86b74642eea15bdac0
Sahib J. Leo <sahib.alejandro@gmail.com>
2014-06-14 17:47:23 -0500

    testing forge quick deploy

--- app/routes.api.php
+++ app/routes.api.php
@@ -1,9 +1,13 @@
 <?php
 Route::group(['prefix' => 'api'], function () {
+
     Route::resource('sentences', 'SentencesAPIController', [
         'only' => ['index', 'store', 'show', 'update', 'destroy']
     ]);
+
+
     Route::resource('tags', 'TagsAPIController', [
         'only' => ['index', 'store', 'show', 'update', 'destroy']
     ]);
+
 });
